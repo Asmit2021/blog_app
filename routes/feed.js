3632352026c30d3fa62c4b8b24bd25f7fdd8e49d
Asmit2021 <2140149@sliet.ac.in>
2024-07-28 10:11:03 +0530

    Complete

---
+++ routes/feed.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const { body } = require('express-validator');
+
+const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
+
+const router = express.Router();
+
+// GET /feed/posts
+router.get('/posts', isAuth, feedController.getPosts);
+
+// POST /feed/post
+router.post('/post',
+    isAuth,
+    [
+    body('title').trim().isLength({min: 5}).withMessage('Title must be at least 5 characters long'),
+    body('content').trim().isLength({min: 5}).withMessage('Content must be at least 5 characters long')
+], feedController.createPost);
+
+router.get('/post/:postId', isAuth,feedController.getPost);
+
+router.put('/post/:postId',
+    isAuth,
+    [
+    body('title').trim().isLength({min: 5}).withMessage('Title must be at least 5 characters long'),
+    body('content').trim().isLength({min: 5}).withMessage('Content must be at least 5 characters long')
+],feedController.updatePost);
+
+router.delete('/post/:postId', isAuth, feedController.deletePost);
+
+module.exports = router;
